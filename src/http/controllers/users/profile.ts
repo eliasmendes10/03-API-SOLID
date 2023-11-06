c23b80a1e1cc098cd24a06a805e3310d4bf6fd1e
Elias <eliasmdes@gmail.com>
2023-11-06 09:51:30 -0300

    Wip Integration tests

--- src/http/controllers/users/profile.ts
+++ src/http/controllers/users/profile.ts
@@ -1,19 +1,16 @@
-// import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  // const getUserProfile = makeGetUserProfileUseCase()
-  console.log(request.headers)
-  await request.jwtVerify()
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  // const { user } = await getUserProfile.execute({
-  //   userId: request.user.sub,
-  // })
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
   return reply.status(200).send({
     user: {
-      // ...user,
+      ...user,
       password_hash: undefined,
     },
   })
